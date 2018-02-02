c74fd9a0700518eb8880ee2021eec7c77f27c4b5
Monica Tamaru Jones <mntamaru@gmail.com>
2018-02-02 17:03:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array[0].to_s
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    i=0
+    str = ""
+    while i < array.size-1
+      str << array[i] + ", "
+      i +=1
+    end
+    str = str + "and " + array[array.size-1]
+  end
 end
